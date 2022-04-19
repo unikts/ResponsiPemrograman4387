4be9f70700d75de235d6f94462c12a271a9b6e22
Unik Trisetyowati <101569348+unikts@users.noreply.github.com>
2022-04-19 12:15:30 +0700

    responsi

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4387
+{
+    class ResponsiPemrograman4387
+    {
+        static void Main(string[] args)
+        {
+            //objek
+            karyawan karyawan = new karyawan();
+            karyawan karyawan1 = new karyawan();
+
+            //nilai 
+            karyawan.No = "1";
+            karyawan.Nik = "190302123";
+            karyawan.Nama = "Paijo";
+            karyawan.GajiBulanan = "3000000";
+
+            Console.WriteLine("Asyik kenaikan gaji 10 persen!");
+
+            karyawan1.No = "2";
+            karyawan1.Nik = "190302124";
+            karyawan1.Nama = "Jono";
+            karyawan1.GajiBulanan = "2000000";
+
+            //pemanggilan
+            karyawan.Info();
+            karyawan.Gaji();
+            karyawan1.Info();
+            karyawan1.Gaji();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4387
+{
+    public class karyawan
+    {
+        //properties
+        public string No { get; set; }
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int NaikGaji { get; set; }
+
+
+        // methods
+        public void Info()
+        {
+            
+            Console.WriteLine("Informasi Karyawan");
+            Console.WriteLine("------------------");
+            Console.WriteLine("No: {0}", No);
+            Console.WriteLine("Nik: {0}", Nik, Nama, GajiBulanan);
+            Console.WriteLine("Nama: {0}", Nama, GajiBulanan);
+            Console.WriteLine("Gaji Bulanan: {0}", GajiBulanan);
+
+            Console.WriteLine("Informasi Karyawan");
+            Console.WriteLine("------------------");
+            Console.WriteLine("No: {0}", No);
+            Console.WriteLine("Nik: {0}", Nik, Nama, GajiBulanan);
+            Console.WriteLine("Nama: {0}", Nama, GajiBulanan);
+            Console.WriteLine("Gaji Bulanan: {0}", GajiBulanan);
+        }
+
+        public void Gaji()
+        {
+            
+            if (Gaji < 0)
+            {
+                this.GajiBulanan = 0;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+                NaikGaji = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            Console.WriteLine("Informasi Karyawan");
+            Console.WriteLine("------------------");
+            Console.WriteLine("No: {0}", No);
+            Console.WriteLine("Nik: {0}", Nik, Nama, GajiBulanan);
+            Console.WriteLine("Nama: {0}", Nama, GajiBulanan);
+            Console.WriteLine("Gaji Bulanan: {0}", GajiBulanan);
+        }
+    }
+}
